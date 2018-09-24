a9bd47a2e02a921dfd816bb5aad920d0d7c56e8a
Luis César Alves <luis_xxl@live.com>
2018-09-23 22:47:23 -0300

    Upload Init

---
+++ src/ApplicationCore/Entity/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entity
+{
+    public class Client
+    {
+        public Client()
+        {
+
+        }
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string CPF { get; set; }
+    }
+}
